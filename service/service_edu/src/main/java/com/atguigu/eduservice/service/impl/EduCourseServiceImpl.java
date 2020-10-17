6c74b093c7205bf4b0288dd2f582d4452f16a0ac
Zetao Zhu <georgezzt@outlook.com>
2020-10-17 13:12:42 -0400

    Day 7 finished

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduCourseServiceImpl.java
@@ -0,0 +1,58 @@
+package com.atguigu.eduservice.service.impl;
+
+import com.atguigu.eduservice.entity.EduCourse;
+import com.atguigu.eduservice.entity.EduCourseDescription;
+import com.atguigu.eduservice.entity.vo.CourseInfoVo;
+import com.atguigu.eduservice.mapper.EduCourseMapper;
+import com.atguigu.eduservice.service.EduCourseDescriptionService;
+import com.atguigu.eduservice.service.EduCourseService;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 课程 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-10-16
+ */
+@Service
+public class EduCourseServiceImpl extends ServiceImpl<EduCourseMapper, EduCourse> implements EduCourseService {
+
+    @Autowired
+    private EduCourseDescriptionService courseDescriptionService;
+
+    @Override
+    public String saveCourseInfo(CourseInfoVo courseInfoVo) {
+
+        //1 向课程表添加课程的基本信息
+        //CourseInfoVo对象转换eduCourse对象
+        EduCourse eduCourse = new EduCourse();
+        BeanUtils.copyProperties(courseInfoVo, eduCourse);
+        int insert = baseMapper.insert(eduCourse);
+
+        if(insert == 0){
+            throw new GuliException(20001, "Failed to add course Info");
+        }
+
+        //获取添加之后的id
+        String cid = eduCourse.getId();
+
+
+
+        //2 向课程简介表添加课程简介
+        // edu_course_description
+        EduCourseDescription courseDescription = new EduCourseDescription();
+        courseDescription.setDescription(courseInfoVo.getDescription());
+        //手动设置下描述id
+        courseDescription.setId(cid);
+        courseDescriptionService.save(courseDescription);
+
+        return cid;
+
+    }
+}
